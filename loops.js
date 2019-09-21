38a72d65ca689ebbf5a7e2dd268bfc0a3dd1bb6a
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-21 19:50:31 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,16 +15,3 @@ function whileLoop(n){
   }
   return 'done'
 }
-
-function doWhileLoop(num){
-    var num = 0;
-
-    function incrementVariable() {
-    num = num + 1;
-    return num;
-  }
-
-  do {console.log("I run once regardless")}
-    while (incrementVariable()) < num)
-
-}
