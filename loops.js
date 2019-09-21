16229df002bf96c375909ebb28ce0d9b9fab6d78
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-21 19:35:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array){
 for (var i = 0; i < 25; i++) {
-  return ["I am ${i} strange loop."]
+  if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
 }
 }
